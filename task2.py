6443b04727cb0f4e55d16f91868df24aad449694
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-09-08 12:25:17 +0530

    task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+# Assignment 1 - Module 2: Basic Python Concepts
+# Task 2: Personalized Greeting
+# Name - Pardeep
+
+# Step 1: Take input from user
+first_name = "Pardeep"
+last_name = "Kumar"
+
+# Concatenate names
+full_name = first_name + " " + last_name
+
+# Step 3: Print personalized greeting
+print(f"\nHello, {full_name} ! Welcome to the Python program.")
