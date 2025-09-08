dbf8a2659f686774546def5340ab85fca86f3a19
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-09-08 12:24:52 +0530

    task1.py

---
+++ task1.py
@@ -0,0 +1,28 @@
+# Assignment 1 - Module 2: Basic Python Concepts
+# Task 1: Perform Basic Mathematical Operations
+# Name - Pardeep
+
+# Also Check My screenshot
+a = 24
+b = 12
+
+# Addition
+print("# Addition")
+print(a + b)
+
+# Subtraction (use \n to get this value below of first if need front so we can use \t)
+print("\n# Subtraction")
+print(a - b)
+
+# Multiplication
+print("\n# Multiplication")
+print(a * b)
+
+# Division (float result with decimal)
+print("\n# Division")
+print(a / b)
+
+# Division (integer result without decimal, using // to time subtract symbol)
+print("\n# Division (without decimal)")
+print(a // b)
+
